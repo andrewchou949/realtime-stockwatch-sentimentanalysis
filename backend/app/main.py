7d59f2a1405bb6528532e73ef0cd434144c34121
Andrew <andrewchou949@gmail.com>
2026-02-05 17:06:32 -0800

    Restructure file arch

--- backend/app/main.py
+++
@@ -1,8 +0,0 @@
-# for registering router
-
-from fastapi import FastAPI
-from app.api.health import router as health_router
-
-app = FastAPI(title="Stock Watch API")
-
-app.include_router(health_router)
